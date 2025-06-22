0c1770cf10bc89217de2a8f32154d50c5cbe9838
Stephen Doe <donsimon505@gmail.com>
2025-06-22 23:37:09 +0000

    Changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     document.addEventListener('DOMContentLoaded', function() {
             
         // Select DOM Elements
-        const addButton = document.getElementById('add-button');
+        const addButton = document.getElementById('add-task-btn');
         const taskInput = document.getElementById('task-input');
         const taskList = document.getElementById('task-list');
             
